ec2d51785941d84d8c9c8fe892783c0eb4144feb
Polzovat123 <sa8.sab@yandex.ru>
2024-04-03 12:52:02 +0500

    clear code

--- etna/transforms/outliers/base.py
+++ etna/transforms/outliers/base.py
@@ -85,17 +85,16 @@ class OutliersTransform(ReversibleTransform, ABC):
         :
             The fitted transform instance.
         """
-        if (
-            self.ignore_flag_column is not None
-            and self.ignore_flag_column not in ts.columns.get_level_values(1).values.tolist()
-        ):
-            raise ValueError(f'Name ignore_flag_column="{self.ignore_flag_column}" not find.')
 
-        s = ts[..., self.ignore_flag_column].isin([0, 1]).all(axis=0)
-        if self.ignore_flag_column is not None and not all(s):
-            raise ValueError(
-                f'Columns ignore_flag contain non binary value: columns: "{self.ignore_flag_column}" in segment: {s[~s].index.get_level_values(0).tolist()}'
-            )
+        if self.ignore_flag_column is not None:
+            if self.ignore_flag_column not in ts.columns.get_level_values(1).values.tolist():
+                raise ValueError(f'Name ignore_flag_column="{self.ignore_flag_column}" not find.')
+            s = ts[..., self.ignore_flag_column].isin([0, 1]).all(axis=0)
+            if not all(s):
+                raise ValueError(
+                    f'Columns ignore_flag contain non binary value: columns: "{self.ignore_flag_column}" in segment: {s[~s].index.get_level_values(0).tolist()}'
+                )
+
         self.segment_outliers = self.detect_outliers(ts)
         self._fit_segments = ts.segments
         super().fit(ts=ts)
