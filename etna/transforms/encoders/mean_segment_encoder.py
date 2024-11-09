Fix `target` leakage in `MeanSegmentEncoderTransform` (#503)
* fix MeanEncoder

* update changelog

* fix segment encoder tests

* make attributes private

---------

Co-authored-by: Egor Baturin <egoriyaa@github.com>

etna/transforms/encoders/mean_segment_encoder.py
@@ -1,24 +1,34 @@
-import reprlib
-from typing import Dict
 from typing import List
-from typing import Optional
 
 import numpy as np
 import pandas as pd
 
 from etna.transforms import IrreversibleTransform
-from etna.transforms.math.statistics import MeanTransform
+from etna.transforms.encoders.mean_encoder import MeanEncoderTransform
 
 
 class MeanSegmentEncoderTransform(IrreversibleTransform):
     """Makes expanding mean target encoding of the segment. Creates column 'segment_mean'."""
 
-    idx = pd.IndexSlice
+    _segment_column = "segment_column"
+    out_column = "segment_mean"
 
     def __init__(self):
         super().__init__(required_features=["target"])
-        self.mean_encoder = MeanTransform(in_column="target", window=-1, out_column="segment_mean")
-        self.global_means: Optional[Dict[str, float]] = None
+        self._mean_encoder = MeanEncoderTransform(
+            in_column=self._segment_column, mode="per-segment", out_column=self.out_column, smoothing=0
+        )
+
+    def _add_segment_column(self, df):
+        segments = df.columns.get_level_values("segment").unique()
+        flatten_segments = np.repeat(segments.values[np.newaxis, :], len(df), axis=0)
+        segment_values = pd.DataFrame(
+            data=flatten_segments,
+            columns=pd.MultiIndex.from_product([segments, [self._segment_column]]),
+            index=df.index,
+        )
+        df = pd.concat([df, segment_values], axis=1).sort_index(axis=1)
+        return df
 
     def _fit(self, df: pd.DataFrame) -> "MeanSegmentEncoderTransform":
         """
@@ -34,10 +44,8 @@ class MeanSegmentEncoderTransform(IrreversibleTransform):
         :
             Fitted transform
         """
-        self.mean_encoder._fit(df)
-        mean_values = df.loc[:, self.idx[:, "target"]].mean().to_dict()
-        mean_values = {key[0]: value for key, value in mean_values.items()}
-        self.global_means = mean_values
+        df = self._add_segment_column(df)
+        self._mean_encoder._fit(df)
         return self
 
     def _transform(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -61,25 +69,11 @@ class MeanSegmentEncoderTransform(IrreversibleTransform):
         NotImplementedError:
             If there are segments that weren't present during training.
         """
-        if self.global_means is None:
-            raise ValueError("The transform isn't fitted!")
-
-        segments = df.columns.get_level_values("segment").unique().tolist()
-        new_segments = set(segments) - self.global_means.keys()
-        if len(new_segments) > 0:
-            raise NotImplementedError(
-                f"This transform can't process segments that weren't present on train data: {reprlib.repr(new_segments)}"
-            )
-
-        df = self.mean_encoder._transform(df)
-        segment = segments[0]
-        nan_timestamps = df[df.loc[:, self.idx[segment, "target"]].isna()].index
-        values_to_set = np.array([self.global_means[x] for x in segments])
-        # repetition isn't necessary for pandas >= 1.2
-        values_to_set = np.repeat(values_to_set[np.newaxis, :], len(nan_timestamps), axis=0)
-        df.loc[nan_timestamps, self.idx[:, "segment_mean"]] = values_to_set
-        return df
+        df = self._add_segment_column(df)
+        df_transformed = self._mean_encoder._transform(df)
+        df_transformed = df_transformed.drop(columns=[self._segment_column], level="feature")
+        return df_transformed
 
     def get_regressors_info(self) -> List[str]:
         """Return the list with regressors created by the transform."""
-        return ["segment_mean"]
+        return [self.out_column]

tests/test_transforms/test_encoders/test_segment_encoder_transform.py
@@ -2,21 +2,20 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from etna.datasets import TSDataset
 from etna.transforms import SegmentEncoderTransform
 from tests.test_transforms.utils import assert_transformation_equals_loaded_original
 from tests.utils import select_segments_subset
 
 
-def test_segment_encoder_transform(simple_ts):
+def test_segment_encoder_transform(mean_segment_encoder_ts):
     transform = SegmentEncoderTransform()
-    transformed_df = transform.fit_transform(simple_ts).to_pandas()
+    transformed_df = transform.fit_transform(mean_segment_encoder_ts).to_pandas()
     assert (
         len(transformed_df.loc[:, pd.IndexSlice[:, "segment_code"]].columns) == 2
     ), "Number of columns not the same as segments"
-    assert len(simple_ts.to_pandas()) == len(transformed_df), "Row missing"
+    assert len(mean_segment_encoder_ts.to_pandas()) == len(transformed_df), "Row missing"
     codes = set()
-    for segment in simple_ts.segments:
+    for segment in mean_segment_encoder_ts.segments:
         column = transformed_df.loc[:, pd.IndexSlice[segment, "segment_code"]]
         assert column.dtype == "category", "Column type is not category"
         assert np.all(column == column.iloc[0]), "Values are not the same for the whole column"
@@ -24,32 +23,15 @@ def test_segment_encoder_transform(simple_ts):
     assert codes == {0, 1}, "Codes are not 0 and 1"
 
 
-def test_subset_segments(simple_ts):
-    train_ts = simple_ts
-    test_df = simple_ts.loc[:, pd.IndexSlice["Omsk", :]]
-    test_ts = TSDataset(df=test_df, freq=simple_ts.freq)
-    transform = SegmentEncoderTransform()
-
-    transform.fit(train_ts)
-    transformed_test_df = transform.transform(test_ts).to_pandas()
-
-    segments = sorted(transformed_test_df.columns.get_level_values("segment").unique())
-    features = sorted(transformed_test_df.columns.get_level_values("feature").unique())
-    assert segments == ["Omsk"]
-    assert features == ["exog", "segment_code", "target"]
-    values = transformed_test_df.loc[:, pd.IndexSlice[:, "segment_code"]]
-    assert np.all(values == values.iloc[0])
-
-
-def test_not_fitted_error(simple_ts):
+def test_not_fitted_error(mean_segment_encoder_ts):
     encoder = SegmentEncoderTransform()
     with pytest.raises(ValueError, match="The transform isn't fitted"):
-        encoder.transform(simple_ts)
+        encoder.transform(mean_segment_encoder_ts)
 
 
-def test_new_segments_error(simple_ts):
-    train_ts = select_segments_subset(ts=simple_ts, segments=["Moscow"])
-    test_ts = select_segments_subset(ts=simple_ts, segments=["Omsk"])
+def test_new_segments_error(mean_segment_encoder_ts):
+    train_ts = select_segments_subset(ts=mean_segment_encoder_ts, segments=["segment_0"])
+    test_ts = select_segments_subset(ts=mean_segment_encoder_ts, segments=["segment_1"])
     transform = SegmentEncoderTransform()
 
     transform.fit(train_ts)